7a863f8fcc044f8431bb6d4a69d579c37d5ae206
r1317 <122949375+r-1317@users.noreply.github.com>
2023-10-16 10:46:27 +0900

    修正

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import fractions
 
 # [機能A]@アリス担当 #############
 def func_A ():
-  print('func_A called.')
+  f = fractions.Fraction
+  t1 = f(2,3)
+  t2 = f(4,6)
+  ans = t1 + t2
+  print(f'{t1} + {t2} = {ans} (= {float(ans):.2f})')
 
 # [機能B]@ボブ担当 ###############
 def func_B ():
@@ -20,12 +24,7 @@ if __name__ == "__main__":
   print("start.")
 
   ## 機能Aの実行
-def func_A ():
-  f = fractions.Fraction
-  t1 = f(2,3)
-  t2 = f(4,6)
-  ans = t1 + t2
-  print(f'{t1} + {t2} = {ans} (= {float(ans):.2f})')
+  func_A ()
   
   ## 機能Bの実行
   func_B()
