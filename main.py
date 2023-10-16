161154f3f3256aa7f188e0e081876512b0ed54b0
Kyushokugorira <122949364+Kyushokugorira@users.noreply.github.com>
2023-10-16 10:24:12 +0900

    ボブが機能Bのスタブを作成

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import math
+import datetime
 
 # [機能A]@アリス担当 #############
 
 
 # [機能B]@ボブ担当 ###############
+def func_B ():
+  print('func_B called.')
 
-
-# メイン処理 #####################
+# メイン処理 ####################
 if __name__ == "__main__": 
 
   print("start.")
@@ -14,5 +16,6 @@ if __name__ == "__main__":
   ## 機能Aの実行
 
   ## 機能Bの実行
+  func_B()
 
   print("end.")
